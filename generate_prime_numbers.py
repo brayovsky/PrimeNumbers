5f7c67d8001bc7689647b49946a3050bade32da8
brayovsky <muthuimbrian@gmail.com>
2017-01-17 13:26:36 +0300

    Handled edge cases where input parameter is not an integer or is a negative number

--- generate_prime_numbers.py
+++ generate_prime_numbers.py
@@ -6,8 +6,18 @@
 2. Iterate from 2 to (y/2)-1 and check the modulus of each number with y
 3. If modulus = 0, break. If modulus is always above 0, add the number y to a return list
 """
+class  NegativeNumberError(Exception):
+	def __init__(self, message):
+		# super( NegativeNumber, self).__init__()
+		self.message = message
+		
 
 def generate_prime_numbers(last_number):
+	if(type(last_number) != int):
+		raise TypeError("Please pass an integer as a parameter")
+	if(last_number<0):
+		raise NegativeNumberError("Please pass a positive integer")
+
 	list_of_prime_numbers = []
 	number = 0
 	while (number<last_number):
@@ -27,3 +37,7 @@ def generate_prime_numbers(last_number):
 					list_of_prime_numbers.append(number)
 		number+=1
 	return list_of_prime_numbers
+
+print(generate_prime_numbers(35))
+
+print(generate_prime_numbers("-35"))
