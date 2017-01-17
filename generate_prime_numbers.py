135a008a7636cc526b4c785bbd774209f8b9e6ec
brayovsky <muthuimbrian@gmail.com>
2017-01-17 12:45:39 +0300

    Integrated the working module for determining if a number is prime into the main function for producing prime numbers

--- generate_prime_numbers.py
+++ generate_prime_numbers.py
@@ -7,41 +7,23 @@
 3. If modulus = 0, break. If modulus is always above 0, add the number y to a return list
 """
 
-# def generate_prime_numbers(last_number):
-# 	print("called")	
-# 	list_of_prime_numbers = []
-# 	start_number = 2
-# 	while (start_number<last_number):
-# 		for divisor in range(2,start_number):
-# 			print (divisor)
-# 		start_number+=1
-
-def determine_prime_number(number):
-	
-	#2 is a special case prime number
-	#the loop can only contain numbers whose truncated  value is more than 1, 
-	#therefore introduce special case for 1 and 3
-	if(number==2 or number ==3):
-		print("Prime number")
-		return ""
-	elif(number==1):
-		print("Not prime number")
-		return""
-	else:
-		half_number = int(number/2)+1
-		for divisor in range(2,half_number):
-			#if any modulus is equal to zero break, not prime
-			#if no modulus is equal to zero, it is prime
-			if(number%divisor==0):
-				print ("Not prime number")
-				break
-			#see if loop has reached last number
-			if(divisor==half_number-1):
-				print("Prime number")
-
-
-determine_prime_number(5)
-determine_prime_number(4)
-determine_prime_number(3)
-determine_prime_number(1)
-# generate_prime_numbers(5)
+def generate_prime_numbers(last_number):
+	list_of_prime_numbers = []
+	number = 0
+	while (number<last_number):
+		if(number==2 or number ==3):
+			list_of_prime_numbers.append(number)
+		elif(number==1 or number == 0):
+			pass
+		else:
+			half_number = int(number/2)+1
+			for divisor in range(2,half_number):
+				#if any modulus is equal to zero break, not prime
+				#if no modulus is equal to zero, it is prime
+				if(number%divisor==0):
+					break
+				#see if loop has reached last number
+				if(divisor==half_number-1):
+					list_of_prime_numbers.append(number)
+		number+=1
+	return list_of_prime_numbers
