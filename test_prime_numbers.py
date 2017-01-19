ab66596747e0155827d327d896df2f2461b721a3
brayovsky <muthuimbrian@gmail.com>
2017-01-19 18:13:17 +0300

    Remove debugging code

--- test_prime_numbers.py
+++ test_prime_numbers.py
@@ -42,14 +42,5 @@ class TestPrimeNumberGeneration(unittest.TestCase):
     	with self.assertRaises(TypeError):
     		generate_prime_numbers.generate_prime_numbers(None)
 
-
-
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
-
 if __name__ == '__main__':
     unittest.main()
